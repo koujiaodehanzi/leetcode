62bf6a4d3fc8c93ad46973df773c316e47bf70a2
wuyankun <wuyankun@lvmama.com>
2019-06-10 13:46:31 +0800

    1 + 2

--- src/main/java/com/wyk/algorithm/leetcode/question0100/Num0001.java
+++ src/main/java/com/wyk/algorithm/leetcode/question0100/Num0001.java
@@ -1,5 +1,8 @@
 package com.wyk.algorithm.leetcode.question0100;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author wuyankun
  * @title: Num0001
@@ -9,6 +12,19 @@ package com.wyk.algorithm.leetcode.question0100;
  */
 public class Num0001 {
 
+    public static void main(String[] args) {
 
+    }
+
+    public static int[] twoNumSum(int[] arr, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i=0; i<arr.length; i++){
+            if (map.containsKey(target-arr[i])){
+                return new int[]{map.get(target-arr[i]), arr[i]};
+            }
+            map.put(arr[i], i);
+        }
+        return null;
+    }
 
 }
